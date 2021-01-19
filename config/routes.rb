272af7b9e0985587346720680c4eecb0498fdd66
YoshitakaKurahashi <72341088+YoshitakaKurahashi@users.noreply.github.com>
2021-01-19 20:48:27 +0900

    画面の微調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "proposals#index"
+  resources :users, only: [:edit, :update]
   resources :proposals do
     resources :comments, only: :create
   end
