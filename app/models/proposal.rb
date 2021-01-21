320bc971bd27dc9c5949088f68c42a201d471d36
YoshitakaKurahashi <72341088+YoshitakaKurahashi@users.noreply.github.com>
2021-01-21 20:23:13 +0900

    proposalバリデーション設定

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -3,5 +3,16 @@ class Proposal < ApplicationRecord
   has_many :users, through: :user_proposals
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  # has_one_attached :image
+
+  with_options presence: true do
+    validates :title,       length: { maximum: 20 }  
+    validates :detail,      length: { maximum: 250 }  
+    validates :term_start
+    validates :term_end
+    validates :target_group 
+    validates :numeric
+    validates :budget,      numericality: { only_integer: true }
+    validates :payment
+  end
+
 end
