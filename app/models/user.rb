acc8b36c59b0a74e37ea7bcbac23adec66a74c60
YoshitakaKurahashi <72341088+YoshitakaKurahashi@users.noreply.github.com>
2021-02-03 20:42:39 +0900

    バリデーション修正（name）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+  validates :name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' }
   # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   # validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :phone_number, format: { with: /\A\d{10,11}\z/ }
